3712e626c04cff28b903deb67f2354b13af4a5c2
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-11 09:59:07 -0300

    aplicando responsividade no componente Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,28 @@
+import { useState } from 'react'
+import CloseIcon from '../../assets/close.svg'
+import MenuIcon from '../../assets/menu.svg'
 import './styles.css'
 
 function Menu() {
+    const [open, setOpen] = useState(false)
     return(
-        <div>
+        <div className={`${open && 'fullscreen-menu'}`}>
+            {open ?
+                <img 
+                 className="icon-close"
+                 style={{display: `${open} ? 'block' : 'none' `}}
+                 src={CloseIcon}
+                 alt="close icon"
+                 onClick={() => setOpen(false)}
+                />                
+                :
+                <img
+                 className='icon-menu'
+                 src={MenuIcon}
+                 alt="menu icon"
+                 onClick={()=> setOpen(true)}
+                />
+            }            
             <ul className='menu'>
                 <li>Ação</li>
                 <li>Comédia</li> 
