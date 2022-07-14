bf3ef0c802c7a638403bfbd60e6214d2750748b5
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-14 18:06:26 -0300

    <style>: <correção de formato>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,15 @@
-import './styles.css'
+import "./styles.css";
 
-function Card( {title, cover }) {
-    return(
-        <div
-         className="container-card"
-         style={{backgroundImage: `url(${cover})`}}
-         >
-             <div className="container-title">
-                 <h1>{title}</h1>
-             </div>
-        </div>
-    )
+function Card({ title, cover }) {
+  return (
+    <div
+      className="container-card"
+      style={{ backgroundImage: `url(${cover})` }}
+    >
+      <div className="container-title">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  );
 }
 export default Card;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,16 @@
-import './styles.css';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import Card from '../../components/Card';
-import { useEffect, useState } from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import "./styles.css";
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Card from "../../components/Card";
 
 function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    async function loadMovieData(){ 
-      const response = await fetch('https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false',{
-        method: 'GET'
+    async function loadMovieData() {
+      const response = await fetch("https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false", {
+        method: "GET",
       });
 
       const { results } = await response.json();
@@ -17,17 +18,17 @@ function Home() {
     }
 
     loadMovieData();
-  },[]);
+  }, []);
 
   return (
-    <div className='container-home'>
+    <div className="container-home">
       <Header />
       <section className="section-movies">
         {movies.map((movie) => (
-          <Card 
-            key={movie.id} 
-            cover={ movie.poster_path } 
-            title={ movie.title}
+          <Card
+            key={movie.id}
+            cover={movie.poster_path}
+            title={movie.title}
           />
         ))}
       </section>
